b3ae44ce608758a017290336e157d5a6b3bf7cb2
Galen Guyer <galen@galenguyer.com>
2023-03-28 12:57:51 -0400

    fix formatting, fix debian package lists

--- manifests/init.pp
+++ manifests/init.pp
@@ -15,7 +15,7 @@
 #   use node encrypt when running tailscale up.  This requires a puppetserver and node encrypt
 # @example
 #   include tailscale
-class tailscale(
+class tailscale (
   Variant[String, Sensitive[String]] $auth_key,
   Stdlib::HttpUrl $base_pkg_url,
   Boolean $manage_package = true,
@@ -25,39 +25,44 @@ class tailscale(
   Boolean $use_node_encrypt = false
 ) {
   if $manage_package_repository {
-  case $::facts[osfamily] {
-    'Debian': {
-      apt::source { 'tailscale':
-        comment  => 'Tailscale packages for ubuntu',
-        location => $base_pkg_url,
-        require  => Apt_key['tailscale'],
-        before   => Package['tailscale']
-
+    case $facts['os']['family'] {
+      'Debian': {
+        if (($facts['os']['distro']['id'] == 'Debian' and $facts['os']['release']['major'] >= '11')
+        or ($facts['os']['distro']['id'] == 'Ubuntu' and $facts['os']['release']['major'] >= '20.04')) {
+          $key_source = "https://pkgs.tailscale.com/stable/${downcase($facts['os']['distro']['id'])}/${facts['os']['distro']['codename']}.noarmor.gpg"
+        } else {
+          $key_source = "https://pkgs.tailscale.com/stable/${downcase($facts['os']['distro']['id'])}/${facts['os']['distro']['codename']}.asc"
+        }
+        apt::source { 'tailscale':
+          comment  => "Tailscale packages for ${facts['os']['distro']['id']} ${facts['os']['distro']['codename']}",
+          location => "https://pkgs.tailscale.com/stable/${downcase($facts['os']['distro']['id'])}",
+          release  => $facts['os']['distro']['codename'],
+          repos    => 'main',
+          key      => {
+            'id'     => '2596A99EAAB33821893C0A79458CA832957F5868',
+            'source' => $key_source,
+          },
+          before   => Package['tailscale'],
+        }
       }
-      apt_key{'tailscale':
-        ensure => present,
-        id     => '2596A99EAAB33821893C0A79458CA832957F5868',
-        source => "${base_pkg_url}/focal.gpg"
+      'RedHat': {
+        yumrepo { 'tailscale-stable':
+          ensure   => 'present',
+          descr    => 'Tailscale stable',
+          baseurl  => "${base_pkg_url}/${facts[operatingsystemmajrelease]}/\$basearch",
+          gpgkey   => "${base_pkg_url}/${facts[operatingsystemmajrelease]}/repo.gpg",
+          enabled  => '1',
+          gpgcheck => '0',
+          target   => '/etc/yum.repo.d/tailscale-stable.repo',
+        }
+      }
+      default: {
+        fail('OS not support for tailscale')
       }
     }
-    'RedHat': {
-      yumrepo { 'tailscale-stable':
-        ensure   => 'present',
-        descr    => 'Tailscale stable',
-        baseurl  => "${base_pkg_url}/${facts[operatingsystemmajrelease]}/\$basearch",
-        gpgkey   => "${base_pkg_url}/${facts[operatingsystemmajrelease]}/repo.gpg",
-        enabled  => '1',
-        gpgcheck => '0',
-        target   => '/etc/yum.repo.d/tailscale-stable.repo',
-      }
-    }
-    default: {
-      fail('OS not support for tailscale')
-    }
-  }
   }
   if $manage_package {
-    package{'tailscale':
+    package { 'tailscale':
       ensure  => present,
     }
   }
@@ -67,15 +72,15 @@ class tailscale(
     $service_provider = undef
   }
   if $manage_service {
-    service{'tailscaled':
-        ensure   => running,
-        enable   => true,
-        provider => $service_provider,
-        require  => [Package['tailscale']]
-      }
+    service { 'tailscaled':
+      ensure   => running,
+      enable   => true,
+      provider => $service_provider,
+      require  => [Package['tailscale']],
+    }
   }
 
-  $up_cli_options =  $up_options.map |$key, $value| { "--${key}=${value}"}.join(' ')
+  $up_cli_options =  $up_options.map |$key, $value| { "--${key}=${value}" }.join(' ')
 
   if $use_node_encrypt {
     # uses node encrypt to unwrap the sensitive value then encrypts it
@@ -87,7 +92,7 @@ class tailscale(
     $ts_command = "tailscale up --authkey=\$SECRET ${up_cli_options}".rstrip
     $env = ["SECRET=${auth_key.unwrap}"]
   }
-  exec{'run tailscale up':
+  exec { 'run tailscale up':
     command     => $ts_command,
     provider    => shell,
     environment => $env,
